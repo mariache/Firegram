a6651de597ced3e37305e17ff5758893eaa4594f
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-04 23:58:15 +1200

    - git init;

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from "./components/Title";
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
+}
+
+export default App;
