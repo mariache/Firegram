835e5a4e4ecc594f3db6815b51f437f33811bfac
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-05 22:47:06 +1200

    - added firebase config; - added firebase npm package;

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDjQ_JZpbBPxOHrF__jwduUTCz9btNBC6Q",
+  authDomain: "firegram-proj.firebaseapp.com",
+  databaseURL: "https://firegram-proj.firebaseio.com",
+  projectId: "firegram-proj",
+  storageBucket: "firegram-proj.appspot.com",
+  messagingSenderId: "895525283931",
+  appId: "1:895525283931:web:40e71fd59e1af0e302b77e",
+  measurementId: "G-K0PSRDQK4K",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
