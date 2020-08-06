8cb86b964f64b0b5d0d9dd9b17f9be12c3a51156
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-06 13:17:18 +1200

    - useEffect for progressBar; - styling for progressBar;

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 
 const ProgressBar = ({ file, setFile }) => {
   const { url, progress } = useStorage(file);
 
-  return (
-    <div className="progress-bar" style={{ width: progress + "%" }}>
-      progress
-    </div>
-  );
+  useEffect(() => {
+    if (url) {
+      setFile(null);
+    }
+  }, [url, setFile]);
+
+  return <div className="progress-bar" style={{ width: progress + "%" }}></div>;
 };
 
 export default ProgressBar;
