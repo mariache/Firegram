1dbdf7aefc957817f2ce7e14d76be1073601a257
Salam Omarbagaev <omarbagaev@gmail.com>
2020-08-07 23:51:51 +1200

    - imageGrid component;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Title from "./components/Title";
 import UploadForm from "./components/UploadForm";
+import ImageGrid from "./components/ImageGrid";
 
 function App() {
   return (
     <div className="App">
       <Title />
       <UploadForm />
+      <ImageGrid />
     </div>
   );
 }
---
+++ src/components/ImageGrid.js
@@ -0,0 +1,19 @@
+import React from "react";
+import useFirestore from "../hooks/useFirestore";
+
+const ImageGrid = ({ setSelectedImg }) => {
+  const { docs } = useFirestore("images");
+
+  return (
+    <div className="img-grid">
+      {docs &&
+        docs.map((doc) => (
+          <div className="img-wrap" key={doc.id}>
+            <img src={doc.url} alt="uploaded pics" />
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default ImageGrid;
